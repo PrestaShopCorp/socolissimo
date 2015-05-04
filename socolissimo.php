609e1d4332a4ec49d10b5654d96ea70a86ee4899
Thomas Nabord <thomas.nabord@prestashop.com>
2015-05-04 09:54:58 +0200

    Update version (v2.9.18)

--- socolissimo.php
+++ socolissimo.php
@@ -64,7 +64,7 @@ class Socolissimo extends CarrierModule
 	{
 		$this->name = 'socolissimo';
 		$this->tab = 'shipping_logistics';
-		$this->version = '2.9.17';
+		$this->version = '2.9.18';
 		$this->author = 'Quadra Informatique';
 		$this->limited_countries = array('fr');
 		$this->module_key = 'faa857ecf7579947c8eee2d9b3d1fb04';
